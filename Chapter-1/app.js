baca985f1a51dc029732b1987a5c4ed207db9606
M-Ishaq-327 <139787290+NajeebKhan111@users.noreply.github.com>
2024-02-24 01:12:11 +0500

    Chapter 1 All tasks done

--- Chapter-1/app.js
+++ Chapter-1/app.js
@@ -1,2 +1,18 @@
 //Display greeting in alert
 alert('hello')
+
+//q2 : Disply the error message
+alert('Error! Pslease enter the valid password.')
+
+//q3 : Display following message on your web page: (Hint : Use line break)
+alert("Welcome to Js Land...\nHappy coding!")
+
+//q4 : Dispay following messages in sequences.
+alert("Welcome to Js Land...")
+alert("Happy Coding!")
+
+//q5 :Generate the following message through browser’s developer console:
+console.log("Hello...I can run Js through my web browser's console.")
+
+
+
