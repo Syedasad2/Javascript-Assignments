bc0a186b6f544501c4455f9f61961bfa3b86f3ef
M-Ishaq-327 <139787290+NajeebKhan111@users.noreply.github.com>
2024-02-24 01:33:03 +0500

    Chapter 2 all task done.

---
+++ Chapter-2/app.js
@@ -0,0 +1,55 @@
+//q1 : Declare a variable called username.
+var username
+
+//q2 : . Declare a variable called myName & assign to it a string that represents your Full Name.
+var myName = "Syed Asad Shah"
+
+//q3: Write script to:
+
+//a) Declare a JS variable, titled message.
+var message
+//b) Assign “Hello World” to variable message
+message = "Hello World"
+//c) Display the message in alert box.
+alert(message)
+
+
+//q4 : . Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+// Save student's bio data in variables
+var nme = "Syed Asad Shah"
+var age = 22
+var occupation = "Certified Web Developer"
+
+// Display the data in alert boxes
+alert("Name: " + nme)
+alert("Age: " + age + " yrs old")
+alert("Occupation: " + occupation)
+
+//q5 : Write a script to display the following alert using one JS variable:
+var pizza = "PIZZA"
+// Step 2: Display the alert step by step now.
+alert(pizza)
+alert(pizza.slice(0, 4))  
+alert(pizza.slice(0, 3))
+alert(pizza.slice(0, 2))
+alert(pizza.slice(0, 1))
+
+// q6 : Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com). Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+var email = "example@example.com"
+alert("My email address is " + email)
+
+// q7 : Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box:
+var book = "A smarter way to learn JavaScript"
+alert("I am trying to learn the book " + book)
+
+// q8 : Write a script to display this in browser through JS.
+document.write("Yah! I can write HTML content through JavaScript")
+
+// q9 : Store following string in a variable and show in alert and browser through JS “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+var myString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(myString);
+document.write(myString);
+
+
+
+
