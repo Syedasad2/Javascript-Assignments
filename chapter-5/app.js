d028501d2eed51246be2b0d83b7bc5b75536a9b6
Syedasad2 <154581585+Syedasad2@users.noreply.github.com>
2024-03-02 12:18:21 +0500

    chapter 5
    chapter 5
    
    Co-Authored-By: syedasad <1492432+syedasad@users.noreply.github.com>

---
+++ chapter-5/app.js
@@ -0,0 +1,39 @@
+var num1 = 40
+var num2 = 20
+
+var addition = num1 + num2
+var subtr = num1 - num2
+var devision = num1 / num2
+var multiply = num1 * num2
+var modulus = 115 % 2
+
+
+console.log(addition)
+console.log(subtr)
+console.log(devision)
+console.log(multiply)
+console.log(modulus)
+
+// post incr
+var pdnum = 50
+var pdnumafter = pdnum++
+
+console.log(pdnum)
+console.log(pdnumafter)
+//post dcr
+var pdnum2 = 30
+var pdnumafter2 = pdnum2--
+console.log(pdnum2)
+console.log(pdnumafter2)
+
+//pre dcr
+var pdnum6 = 60
+var pdnumbefore = --pdnum6
+console.log(pdnum6)
+console.log(pdnumbefore)
+
+//pre incr
+var pdnum3 = 60
+var pdnumbefore2 = ++pdnum3
+console.log(pdnum3)
+console.log(pdnumbefore2)
