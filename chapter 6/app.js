b5e7ccdca814f21b9d22effd574fcf768d4f45c6
Syedasad2 <154581585+Syedasad2@users.noreply.github.com>
2024-03-11 13:56:02 +0500

    chapter 6

---
+++ chapter 6/app.js
@@ -0,0 +1,21 @@
+
+// post decrement/increament
+var num = 10;
+var newnum = num++;
+console.log(num);
+console.log(newnum);
+
+var num = 10;
+var newnum = num--;
+console.log(num);
+console.log(newnum);
+// pre decrement/increament
+var num = 10;
+var newnum = ++num;
+console.log(num);
+console.log(newnum);
+
+var num = 10;
+var newnum = --num;
+console.log(num);
+console.log(newnum);
---
+++ chapter 7/app.js
@@ -0,0 +1,2 @@
+var totalcost = 1 +(20 / 4 * 100)
+console.log(totalcost);
